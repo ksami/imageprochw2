8473d6c7f4717b29de129712ab64cb00e7f25c15
ksami <ksami.ihide@gmail.com>
2015-04-03 17:44:06 +0900

    mix up row and col again

--- findLight.m
+++ findLight.m
@@ -6,23 +6,23 @@ function [lv] = findLight(img, cx, cy, r)
 
 [row, col] = size(img);
 
-% Find brightest pixel (x,y) %
+% Find brightest pixel (y,x) %
 %matrices indexed as
 %1 4 7
 %2 5 8
 %3 6 9
 [val, idx] = max(img(:));
-x = mod(idx, row);
-y = ceil(double(idx)/row);
+y = mod(idx, row);
+x = ceil(double(idx)/row);
 
 % Find normal at brightest pixel %
 %using sphere eqn, set cz = 0 ie. sphere at (cx, cy, 0)
 z = sqrt( (x-cx)^2 + (y-cy)^2 - r^2 );
 
 if (isa(img, 'uint8'))
-    intensity = double(img(x,y))/255;
+    intensity = double(img(y,x))/255;
 else
-    intensity = img(x,y);
+    intensity = img(y,x);
 end
 
 lv = [x y z];
